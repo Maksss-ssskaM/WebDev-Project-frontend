a780837a8a78f6c31483b4901a9d970e19a8d1de
Maks <maks.shein.2002@mail.ru>
2023-04-05 17:32:09 +0300

    Refactoring components - Add types to component - Fix auth screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from "./components/home";
+import Home from "./pages/home";
 import {Route, Routes} from "react-router-dom";
 import LoginPage from "./components/auth/login";
 import RegisterPage from './components/auth/register';
@@ -8,9 +8,9 @@ import AuthRootComponent from "./components/auth";
 import {ColorModeContext, useMode} from "./theme";
 import {CssBaseline, ThemeProvider} from "@mui/material";
 import LayoutComponent from "./components/layout";
-import WatchlistComponent from "./components/watchlist";
-import NewsComponent from "./components/news";
-import SettingsComponent from "./components/settings";
+import WatchlistComponent from "./pages/watchlist";
+import NewsComponent from "./pages/news";
+import SettingsComponent from "./pages/settings";
 
 function App() {
     const [theme, colorMode] = useMode()
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
 import {ILayout} from "../../common/types/layout";
-import TopBarComponent from "../top-bar";
+import TopBarComponent from "../topbar";
 import {useLocation} from "react-router-dom";
 import {Box, useMediaQuery} from "@mui/material";
 import SidebarComponent from "../sidebar";
 import {useStyles} from "./styles";
 
-const LayoutComponent = ({children}: ILayout) => {
+const LayoutComponent: React.FC<ILayout> = ({children}: ILayout): JSX.Element => {
     const[isOpen, setIsOpen] = useState(false)
     const location = useLocation()
     const isNonMobile = useMediaQuery('(min-width: 600px)')
