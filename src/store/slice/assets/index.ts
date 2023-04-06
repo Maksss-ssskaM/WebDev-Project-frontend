4aca0e36c41fd3fc65fe5bbe4e05d4bd9acfddde
Maks <maks.shein.2002@mail.ru>
2023-04-06 19:20:38 +0300

    Fetch data from coingecko - Create fetch logic - Save data to store - Get data from store in home page

---
+++ src/store/slice/assets/index.ts
@@ -0,0 +1,20 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {getPopularAssets} from "../../thunks/assets";
+
+const initialState: any = {
+    assets: [],
+    popularAssets: []
+}
+
+export const assetSlice = createSlice({
+    name: 'assets',
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+        builder.addCase(getPopularAssets.fulfilled, (state, action) => {
+            state.popularAssets.push(action.payload)
+        })
+    }
+})
+
+export default assetSlice.reducer
