af7e28395559f987ddb80ba789e13c691a2984e6
Maks <maks.shein.2002@mail.ru>
2023-04-07 14:44:31 +0300

    Improve area chart component - Fix data length - Add option chart - Add chart data

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,6 +11,8 @@ import { string } from "yup";
 import { Box, Grid } from "@mui/material";
 import { useStyles } from "./styles";
 import AreaChart from "../../components/charts/area-chart";
+import TrendUp from "../../assets/images/charts/trend-up.svg";
+import TrendDown from "../../assets/images/charts/trend-down.svg";
 
 const Home: React.FC = (): JSX.Element => {
     const classes = useStyles();
@@ -43,9 +45,16 @@ const Home: React.FC = (): JSX.Element => {
     );
 
     const renderPopularBlock = filteredArray.map((element: any) => {
-        const currentPrice = element.data.prices[0];
-        const currentCap = element.data.market_caps[0];
-        console.log(popularAssets);
+        const currentPrice = element.singleAsset.map(
+            (element: any) => element.current_price,
+        );
+        const currentCap = element.singleAsset.map(
+            (element: any) => element.market_cap,
+        );
+        const changePrice = element.singleAsset.map(
+            (element: any) => element.price_change_percentage_24h,
+        );
+
         return (
             <Grid item xs={12} md={6} lg={6} key={element.name}>
                 <Grid container className={classes.topCardItem}>
@@ -53,15 +62,33 @@ const Home: React.FC = (): JSX.Element => {
                         <h3 className={classes.assetName}>{element.name}</h3>
                         <div className={classes.itemDetails}>
                             <h3 className={classes.cardPrice}>
-                                ${currentPrice[1].toFixed(2)}
+                                ${currentPrice}
                             </h3>
-                            <p className={classes.cardCapitalize}>
-                                ${currentCap[1].toFixed(0)}
-                            </p>
+                            <div className={classes.additionalInfo}>
+                                <Box
+                                    className={
+                                        changePrice > 0
+                                            ? `${classes.priceTrend} ${classes.trendUp}`
+                                            : `${classes.priceTrend} ${classes.trendDown}`
+                                    }
+                                >
+                                    {changePrice > 0 ? (
+                                        <img src={TrendUp} alt="TrendUp" />
+                                    ) : (
+                                        <img src={TrendDown} alt="TrendDown" />
+                                    )}
+                                    <span>
+                                        %{Number(changePrice).toFixed(2)}
+                                    </span>
+                                </Box>
+                                <div className={classes.cardCapitalize}>
+                                    ${currentCap}
+                                </div>
+                            </div>
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={6} lg={6}>
-                        <AreaChart data={element.data.prices} />
+                        <AreaChart data={element.data} />
                     </Grid>
                 </Grid>
             </Grid>
--- src/store/thunks/assets/index.ts
+++ src/store/thunks/assets/index.ts
@@ -8,7 +8,18 @@ export const getPopularAssets = createAsyncThunk(
             const assets = await coinGeckoApi.get(
                 `/coins/${data}/market_chart?vs_currency=usd&days=90`,
             );
-            return { name: data, data: assets.data };
+            const singleAsset = await coinGeckoApi.get(
+                `/coins/markets?vs_currency=usd&ids=${data}&order=market_cap_desc&per_page=100&page=1&sparkline=false`,
+            );
+            return {
+                name: data,
+                data: assets.data.prices.slice(
+                    // срез датасета цен
+                    assets.data.prices.length - 30, // от старых
+                    assets.data.prices.length - 1, // к новым
+                ),
+                singleAsset: singleAsset.data,
+            };
         } catch (error: any) {
             if (error.response && error.response.data.message) {
                 return rejectWithValue(error.response.data.message);
